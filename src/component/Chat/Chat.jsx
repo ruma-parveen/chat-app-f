efb85286518aa0f4a4ad7ab16b9c6ee48d5cf49c
ruma parveen <rumap631@gmail.com>
2025-07-27 15:41:02 +0530

    Updated UI / Fixed gap issue

--- src/component/Chat/Chat.jsx
+++ src/component/Chat/Chat.jsx
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "https://chat-app-fbk4.onrender.com/";
+const ENDPOINT = process.env.PORT;
 
 const Chat = () => {
   const [id, setid] = useState("");
