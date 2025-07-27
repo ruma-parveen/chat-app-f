e34983bef4e0bfff807aa16f01f7eb72b6277a57
ruma parveen <rumap631@gmail.com>
2025-07-27 15:48:13 +0530

    fix Server

--- src/component/Chat/Chat.jsx
+++ src/component/Chat/Chat.jsx
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = process.env.PORT;
+const ENDPOINT = "https://chat-app-fbk4.onrender.com";
 
 const Chat = () => {
   const [id, setid] = useState("");
